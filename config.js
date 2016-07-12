e6af4a85b9fe2e3cfa5995a2938fb75525401044
George Lambeth <glambeth1594@gmail.com>
2016-07-12 10:38:32 -0500

    DB setup

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
 module.exports = {
 	"account_SID" : "AC78ff70a51e0a985bcf6bba9b734c149f",
-	"AUTH_TOKEN" : "89522d1e3241fa473a094a4bc41a05d1"
+	"AUTH_TOKEN" : "89522d1e3241fa473a094a4bc41a05d1",
+	"database" : "mongodb://root:abc123@ds021462.mlab.com:21462/phonedb",
+	"port" : process.env.PORT || 3000
 };
--- server.js
+++ server.js
@@ -1,4 +1,18 @@
 var config = require('./config.js');
+var express = require('express');
 var request = require('request');
+var mongoose = require('mongoose');
 var twilio = require('twilio')(config.account_SID, config.AUTH_TOKEN);
+var app = express();
 
+mongoose.connect(config.database, function(err) {
+	if (err) {
+		console.log(err);
+	} else {
+		console.log('connected to the DB');
+	}
+});
+
+app.listen(config.port, function(req, res) {
+	console.log('listening on port ' + config.port);
+});
